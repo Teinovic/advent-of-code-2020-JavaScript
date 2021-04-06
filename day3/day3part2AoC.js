21685e9146c1d50b13a918b3204d22eb3262e33c
Teinovic <momir.teinovic.2011@gmail.com>
2021-04-06 23:41:48 +0200

    solution for part2, pretty elegant if you ask me

--- day3/day3part2AoC.js
+++ day3/day3part2AoC.js
@@ -3,10 +3,10 @@ let text = fs.readFileSync("./day3/input.txt").toString();
 let tobogganInput = text.split("\n")
 
 
-function treesCut(start=0, right=3) {
+function treesCut(start=0, right=3, down=1) {
     let positionOnSlope = 0
     let treesCutCounter = 0
-    for (let slopeLocationIndex=start; slopeLocationIndex<tobogganInput.length; slopeLocationIndex++) {
+    for (let slopeLocationIndex=start; slopeLocationIndex<tobogganInput.length; slopeLocationIndex += down) {
         let slope = tobogganInput[slopeLocationIndex]
         if (slope[positionOnSlope] === '#') {
             treesCutCounter += 1
@@ -18,4 +18,8 @@ function treesCut(start=0, right=3) {
     return treesCutCounter
 }
 
-console.log(treesCut())
+function treesCutByFiveTrajectoriesMultiplied() {
+    return treesCut(0, 1) * treesCut(0, 3) * treesCut(0, 5) * treesCut(0, 7) * treesCut(0, 1, 2)
+}
+
+console.log(treesCutByFiveTrajectoriesMultiplied())
